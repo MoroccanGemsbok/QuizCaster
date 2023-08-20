d08606210eb1d8a81af111746e011ffd384660db
Richard Zhu <richard.yunqi.zhu@gmail.com>
2023-08-19 20:05:02 -0400

    openai generation stuff initial

---
+++ desktop/text_generation/preprocessing.py
@@ -0,0 +1,154 @@
+from typing import List
+import re
+
+
+def replace_ligatures(text: str) -> str:
+    ligatures = {
+        "ﬀ": "ff",
+        "ﬁ": "fi",
+        "ﬂ": "fl",
+        "ﬃ": "ffi",
+        "ﬄ": "ffl",
+        "ﬅ": "ft",
+        "ﬆ": "st",
+        "Ꜳ": "AA",
+        "Æ": "AE",
+        "ꜳ": "aa",
+    }
+    for search, replace in ligatures.items():
+        text = text.replace(search, replace)
+    return text
+
+
+def remove_hyphens(text: str) -> str:
+    """
+
+    This fails for:
+    * Natural dashes: well-known, self-replication, use-cases, non-semantic,
+                      Post-processing, Window-wise, viewpoint-dependent
+    * Trailing math operands: 2 - 4
+    * Names: Lopez-Ferreras, VGG-19, CIFAR-100
+    """
+    lines = [line.rstrip() for line in text.split("\n")]
+
+    # Find dashes
+    line_numbers = []
+    for line_no, line in enumerate(lines[:-1]):
+        if line.endswith("-"):
+            line_numbers.append(line_no)
+
+    # Replace
+    for line_no in line_numbers:
+        lines = dehyphenate(lines, line_no)
+
+    return "\n".join(lines)
+
+
+def dehyphenate(lines: List[str], line_no: int) -> List[str]:
+    next_line = lines[line_no + 1]
+    word_suffix = next_line.split(" ")[0]
+
+    lines[line_no] = lines[line_no][:-1] + word_suffix
+    lines[line_no + 1] = lines[line_no + 1][len(word_suffix):]
+    return lines
+
+
+def to_lowercase(text: str) -> str:
+    return text.lower()
+
+
+def remove_newlines(text: str) -> str:
+    return text.replace("\n", " ")
+
+
+alphabets = "([A-Za-z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr|Prof|Capt|Cpt|Lt|Mt)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|St|Capt|Mrs|Ms|Dr|Prof|Capt|Cpt|Lt|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov|me|edu)"
+digits = "([0-9])"
+multiple_dots = r'\.{2,}'
+
+
+# The gigaton function (holy moly)
+def split_into_sentences(text: str) -> list[str]:
+    """
+    Split the text into sentences.
+
+    If the text contains substrings "<prd>" or "<stop>", they would lead 
+    to incorrect splitting because they are used as markers for splitting.
+
+    :param text: text to be split into sentences
+    :type text: str
+
+    :return: list of sentences
+    :rtype: list[str]
+    """
+    text = " " + text + "  "
+    text = text.replace("\n", " ")
+    text = re.sub(prefixes, "\\1<prd>", text)
+    text = re.sub(websites, "<prd>\\1", text)
+    text = re.sub(digits + "[.]" + digits, "\\1<prd>\\2", text)
+    text = re.sub(multiple_dots, lambda match: "<prd>" *
+                  len(match.group(0)) + "<stop>", text)
+    if "Ph.D" in text:
+        text = text.replace("Ph.D.", "Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] ", " \\1<prd> ", text)
+    text = re.sub(acronyms+" "+starters, "\\1<stop> \\2", text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" +
+                  alphabets + "[.]", "\\1<prd>\\2<prd>\\3<prd>", text)
+    text = re.sub(alphabets + "[.]" + alphabets +
+                  "[.]", "\\1<prd>\\2<prd>", text)
+    text = re.sub(" "+suffixes+"[.] "+starters, " \\1<stop> \\2", text)
+    text = re.sub(" "+suffixes+"[.]", " \\1<prd>", text)
+    text = re.sub(" " + alphabets + "[.]", " \\1<prd>", text)
+    if "”" in text:
+        text = text.replace(".”", "”.")
+    if "\"" in text:
+        text = text.replace(".\"", "\".")
+    if "!" in text:
+        text = text.replace("!\"", "\"!")
+    if "?" in text:
+        text = text.replace("?\"", "\"?")
+    if "..." in text:
+        text = text.replace("...", "<prd><prd><prd>")
+    text = text.replace("e.g.", "e<prd>g<prd>")
+    text = text.replace("i.e.", "i<prd>e<prd>")
+    text = text.replace(".", ".<stop>")
+    text = text.replace("?", "?<stop>")
+    text = text.replace("!", "!<stop>")
+    text = text.replace("<prd>", ".")
+    sentences = text.split("<stop>")
+    sentences = [s.strip() for s in sentences]
+    if sentences and not sentences[-1]:
+        sentences = sentences[:-1]
+    return sentences
+
+
+def remove_duplicates(lst):
+    i = 1
+    while i < len(lst):
+        if lst[i] == lst[i - 1]:
+            lst.pop(i)
+        else:
+            i += 1
+    return lst
+
+
+def group_sentences(sentences: List[str], sentences_per_prompt: int) -> List[str]:
+    """
+    Group sentences into prompts.
+
+    :param sentences: list of sentences
+    :type sentences: List[str]
+    :param sentences_per_prompt: number of sentences per prompt
+    :type sentences_per_prompt: int
+
+    :return: list of prompts
+    :rtype: List[str]
+    """
+    prompts = []
+    for i in range(0, len(sentences), sentences_per_prompt):
+        prompts.append(" ".join(sentences[i:i + sentences_per_prompt]))
+    return prompts
