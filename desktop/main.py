eca9598eb61abb6da31f32c7fc78d8842ef5f68c
sammyyyyy1 <samuelzheng234@gmail.com>
2023-08-19 22:38:49 -0400

    hid env variables

--- desktop/main.py
+++ desktop/main.py
@@ -28,9 +28,9 @@ def main():
         threshold = audio_init()
         api = Api(threshold)
         webview.create_window(
-            "QuizCaster", "http://192.168.2.162:3000/",
+            "QuizCaster", "http://localhost:3000/",
             # 192.168.2.162
-            width=1600, height=900, js_api=api
+            width=1600, height=1000, js_api=api
         )
         webview.start()
     except Exception as e:
